add11716d5b039032f3b1a49b2f63c94a9f6266b
Tanguy Raufflet <tanguy.raufflet@savoirfairelinux.com>
2024-05-02 10:09:33 +0200

    cluster_dashboard: add plugin skeleton
    Plugin skeleton for the cluster dashboard.
    
    Signed-off-by: Tanguy Raufflet <tanguy.raufflet@savoirfairelinux.com>

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2017-2024 Savoir-faire Linux Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import React from 'react';
+
+export class Application extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="title1">Cluster Dashboard</h1>
+      </div>
+    );
+  }
+}
