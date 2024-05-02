53a0dae3498dd1085af51321451d86ab4ede26f3
Tanguy Raufflet <tanguy.raufflet@savoirfairelinux.com>
2024-05-02 10:17:53 +0200

    app: add basic html template
    The dashboard is divided in 4 areas: node status, cluster status,
    ressources status and ceph status.
    
    Signed-off-by: Tanguy Raufflet <tanguy.raufflet@savoirfairelinux.com>

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2024 Savoir-faire Linux Inc.
+ * Copyright (C) 2024 Savoir-faire Linux Inc.
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -10,6 +10,28 @@ export class Application extends React.Component {
     return (
       <div>
         <h1 className="title1">Cluster Dashboard</h1>
+        <div className="grid-container">
+          <div className="grid-item">
+            <div>
+              <h2 className="title2">Node Status</h2>
+            </div>
+          </div>
+          <div className="grid-item">
+            <div>
+              <h2 className="title2">Cluster Status</h2>
+            </div>
+          </div>
+          <div className="grid-item">
+            <div>
+              <h2 className="title2">Resources Status</h2>
+            </div>
+          </div>
+          <div className="grid-item">
+            <div>
+              <h2 className="title2">Ceph status</h2>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
