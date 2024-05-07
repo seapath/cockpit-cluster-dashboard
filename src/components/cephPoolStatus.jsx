1d9aaab82e29ab99f5cbd0885c5f623510e94f89
Tanguy Raufflet <tanguy.raufflet@savoirfairelinux.com>
2024-05-07 11:49:16 +0200

    cephPoolStatus: add react component
    The component uses a JSON to get the state of the pool RBD (data stored,
    space used and space available). The result is displayed in a table.
    
    Signed-off-by: Tanguy Raufflet <tanguy.raufflet@savoirfairelinux.com>

---
+++ src/components/cephPoolStatus.jsx
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2024 Savoir-faire Linux Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import cockpit from 'cockpit';
+import React from 'react';
+
+export default class CephPoolStatus extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            stored:0,
+            used:0,
+            available:0,
+        };
+
+        cockpit.spawn(["ceph", "df", "-f", "json"], {superuser: "try"})
+            .then(output => {
+                const outputJSON = JSON.parse(output);
+                const rbd = outputJSON.pools.find(n => n.name === "rbd");
+
+                this.setState({
+                    stored: rbd.stats.stored,
+                    used: rbd.stats.kb_used*1024,
+                    available: rbd.stats.max_avail,
+                });
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                <h4 className="text"> Pool RBD: </h4>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Data stored</th>
+                            <th>Space used</th>
+                            <th>Space available</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{ cockpit.format_bytes(this.state.stored) }</td>
+                            <td>{ cockpit.format_bytes(this.state.used) }</td>
+                            <td>{ cockpit.format_bytes(this.state.available) }</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
