c15525f8014127d03b54365e58831bced97a775e
Tanguy Raufflet <tanguy.raufflet@savoirfairelinux.com>
2024-08-27 16:10:14 +0200

    app: add periodic re-render
    This commit adds the components `updateIntervalSelector` to the webpage
    and the props `lastUpdate` to each child components, which will trigger
    the re-rendering of each of them each time this value had changed
    (i.e. the time defined in the selector has elapsed)
    
    Signed-off-by: Tanguy Raufflet <tanguy.raufflet@savoirfairelinux.com>

--- src/app.jsx
+++ src/app.jsx
@@ -8,38 +8,55 @@ import NodeStatus from './components/nodeStatus';
 import ClusterStatus from './components/clusterStatus';
 import ResourcesStatus from './components/ressourcesStatus';
 import CephStatus from './components/cephStatus';
+import UpdateIntervalSelector from './components/updateIntervalSelector';
 
 export class Application extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      lastUpdate: Date.now(),
+    };
+  }
+
+  handleUpdate = (lastUpdate) => {
+    this.setState({ lastUpdate });
+  };
+
   render() {
     return (
       <div>
         <h1 className="title1">Cluster Dashboard</h1>
-        <div style={{ fontSize: 'small' }}>
-          {"last update: " + new Date().toLocaleString()}
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+          <div style={{ fontSize: 'small' }}>
+            {"last update: " + new Date().toLocaleString()}
+          </div>
+          <UpdateIntervalSelector
+            onUpdate={this.handleUpdate}
+          />
         </div>
         <div className="grid-container">
           <div className="grid-item">
             <div>
               <h2 className="title2">Node Status</h2>
-              <NodeStatus />
+              <NodeStatus lastUpdate={this.state.lastUpdate} />
             </div>
           </div>
           <div className="grid-item">
             <div>
               <h2 className="title2">Cluster Status</h2>
-              <ClusterStatus />
+              <ClusterStatus lastUpdate={this.state.lastUpdate} />
             </div>
           </div>
           <div className="grid-item">
             <div>
               <h2 className="title2">Resources Status</h2>
-              <ResourcesStatus />
+              <ResourcesStatus lastUpdate={this.state.lastUpdate} />
             </div>
           </div>
           <div className="grid-item">
             <div>
               <h2 className="title2">Ceph status</h2>
-              <CephStatus />
+              <CephStatus lastUpdate={this.state.lastUpdate} />
             </div>
           </div>
         </div>
